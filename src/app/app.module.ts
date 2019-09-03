b947c4fac7c315bc0d053cf3876abacbdbb5082d
shironai <huybohoho37@gmail.com>
2019-09-03 14:04:44 +0700

    import HTTPCLIENT MODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 import {CustomMaterialModule} from './core/material.module';
-import { RegistrationComponent } from './signup/registration.component';
-import { SignupComponent } from './signup/signup.component';
+import {SignupComponent} from './signup/signup.component';
+import {HttpClientModule} from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UserComponent,
-    RegistrationComponent,
     SignupComponent
   ],
   imports: [
@@ -22,9 +22,11 @@ import { SignupComponent } from './signup/signup.component';
     BrowserAnimationsModule,
     CustomMaterialModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
