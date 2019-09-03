6f9b94cc732d0469903e866e76b091433087ca43
shironai <huybohoho37@gmail.com>
2019-09-03 16:06:52 +0700

    refactor app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {UserComponent} from './user/user.component';
 import {SignupComponent} from './signup/signup.component';
+import {IndexComponent} from './index/index.component';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'login', component: LoginComponent },
-  {path : 'signup', component : SignupComponent}
+  {path : 'signup', component : SignupComponent},
+  {path: 'index', component: IndexComponent },
+  {path:'', component: IndexComponent}
 ];
 @NgModule({
   imports: [
